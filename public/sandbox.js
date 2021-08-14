5812e2a26939b6e89e9db03342ea8de02986a2ec
Jakub Molicki <molickikuba@gmail.com>
2021-08-14 12:14:18 +0200

    lesson8

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("tffest2");
+// let greet = () => {
+//     console.log('hello world')
+// }
+// greet();
+var greet;
+// greet = "hello"
+greet = function () {
+    console.log("hello, again");
+};
+greet();
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 7);
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(4, 5);
+console.log(result);
+// result = 'kub';
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,33 @@
-console.log("tffest2")
+// let greet = () => {
+//     console.log('hello world')
+// }
+
+// greet();
+
+let greet: Function;
+
+// greet = "hello"
+
+greet = () => {
+    console.log("hello, again")
+}
+greet();
+
+
+
+const add = (a:number, b:number, c: number | string = 10 ) => {
+    console.log(a+b);
+    console.log(c)
+}
+
+add(5,7)
+
+
+const minus = (a: number, b:number): number => {
+    return a + b;
+}
+
+let result = minus(4, 5)
+console.log(result)
+// result = 'kub';
+
