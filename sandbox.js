90df26855bd94149a164daaa323874b445e09a89
Jakub Molicki <molickikuba@gmail.com>
2021-08-14 10:48:03 +0200

    lesson3

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,13 @@
 var character = "mario";
 var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = "luigi";
+// age = 'halo';
+age = 45;
+// isBlackBelt = "halo";
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,18 @@
 let character = "mario";
-
 let age = 30;
+let isBlackBelt = false;
 
+// character = 20;
+character = "luigi";
+
+// age = 'halo';
+age = 45;
+
+// isBlackBelt = "halo";
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(10));
