bfc40a26ff2a77c9bf05a343f2abe0c2f5940a1e
Jakub Molicki <molickikuba@gmail.com>
2021-08-14 18:04:13 +0200

    lesson12

--- public/app.js
+++ public/app.js
@@ -1,9 +1,26 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// // if(anchor){
-// //     console.log(anchor.href);
-// // }
-// console.log(anchor.href);
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario webiste', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi webiste', 350);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// console.log(invoices)
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invoices);
+console.log(invOne.format());
 var form = document.querySelector('.new-item-form');
 // console.log(form.children);
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,37 @@
-// const anchor = document.querySelector('a')!;
+// classes
 
-// // if(anchor){
-// //     console.log(anchor.href);
-// // }
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-// console.log(anchor.href);
+    constructor(c: string, d:string, a:number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario','work on the mario webiste', 250)
+const invTwo = new Invoice('luigi','work on the luigi webiste', 350)
+
+let invoices: Invoice[] = [];
+invoices.push(invOne)
+invoices.push(invTwo)
+// console.log(invoices)
+
+invOne.client = 'yoshi'
+invTwo.amount = 400;
+
+console.log(invoices)
+
+console.log(invOne.format())
+
+
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
